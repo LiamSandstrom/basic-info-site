f14232797ab0e947592488a85fa0509935afa01f
LiamSandstrom <liam@Liams-MacBook-Pro.local>
2026-01-20 11:11:24 +0100

    refactor index.js

--- index.js
+++ index.js
@@ -7,38 +7,35 @@ const port = 8080
 
 const server = createServer(async (req, res) => {
     let path = req.url;
-    res.setHeader("Content-Type", "text/html")
-    let folder = "views";
-    let end = "html";
-    let isCss = false;
 
-    if (isCssRequest(path)) {
-        isCss = true
-        res.setHeader("Content-Type", "text/css")
-    }
+    let isCss = isCssRequest(path);
+    res.setHeader("Content-Type", isCss ? "text/css" : "text/html");
 
     try {
-        if (path === "/") path = "index"
-        const contentPath = isCss ? `.${path}` : `./${folder}/${path}.${end}`;
+        if (path === "/" || path === "/home") path = "index"
+        const contentPath = isCss ? `.${path}` : `./views/${path}.html`;
         const content = await fs.readFile(contentPath, "utf8");
         res.statusCode = 200;
         res.end(content)
     }
     catch {
         res.statusCode = 404;
-        const content = await fs.readFile("./views/404.html", "utf8");
-        res.end(content);
+        try {
+            const content = await fs.readFile("./views/404.html", "utf8");
+            res.end(content);
+        }
+        catch {
+            res.end("404 - Not Found")
+        }
     }
-
 })
 
 server.listen(port, hostname, () => {
     console.log(`server running at http://${hostname}:${port}/`)
 })
 
-
 function isCssRequest(path) {
-    return path.substr(path.length - 3) == "css";
+    return path.endsWith(".css");
 }
 
 
